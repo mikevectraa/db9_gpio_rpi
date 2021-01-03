6073311d7a3b7ab08b40f5c933fd233093556f41
mikevectraa <76874823+mikevectraa@users.noreply.github.com>
2021-01-03 11:53:21 +0100

    Update db9_gpio_rpi.c
    pins changed to use 2 identical dsub9 adapters for player 1 and player 2 on audio+ from raspiaudio.com

--- db9_gpio_rpi-1.2/db9_gpio_rpi.c
+++ db9_gpio_rpi-1.2/db9_gpio_rpi.c
@@ -80,23 +80,23 @@ static u32 db9_bcm_model;
 #define HAVE_TIMER_SETUP
 #endif
 
-//pins changed to avoid conflicts with "raspisound"
+//pins changed to use 2 identical dsub9 adapters for player 1 and player 2 on audio+ from raspiaudio.com
 
 enum pad_gpios {
-	PORT1_PIN1_GPIO = 4,
-	PORT1_PIN2_GPIO = 7,
-	PORT1_PIN3_GPIO = 8,
-	PORT1_PIN4_GPIO = 9,
-	PORT1_PIN5_GPIO = 10,
-	PORT1_PIN6_GPIO = 11,
-	PORT1_PIN7_GPIO = 14,
-	PORT2_PIN1_GPIO = 15,
-	PORT2_PIN2_GPIO = 17,
-	PORT2_PIN3_GPIO = 27,
-	PORT2_PIN4_GPIO = 22,
-	PORT2_PIN5_GPIO = 23,
-	PORT2_PIN6_GPIO = 24,
-	PORT2_PIN7_GPIO = 25
+	PORT1_PIN1_GPIO = 7,
+	PORT1_PIN2_GPIO = 8,
+	PORT1_PIN3_GPIO = 9,
+	PORT1_PIN4_GPIO = 10,
+	PORT1_PIN5_GPIO = 25,
+	PORT1_PIN6_GPIO = 24,
+	PORT1_PIN7_GPIO = 23,
+	PORT2_PIN1_GPIO = 22,
+	PORT2_PIN2_GPIO = 27,
+	PORT2_PIN3_GPIO = 17,
+	PORT2_PIN4_GPIO = 4,
+	PORT2_PIN5_GPIO = 15,
+	PORT2_PIN6_GPIO = 14,
+	PORT2_PIN7_GPIO = 3
 };
 
 /* Port gpio IDs */
